932fa500fd7259a3118779f67fbd4f7379e322a7
viacheslav34 <77201329+viacheslav34@users.noreply.github.com>
2021-12-11 16:28:50 +0300

    доработали статус

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const axios = require('axios');
 const config = require('./config')
 const token = config.token
-const text = encodeURIComponent(getCurrectTime())
+const months = {
+    0: 'Января',
+    1: 'Февраля',
+    2: 'Марта',
+    3: 'Апреля',
+    4: 'Мая',
+    5: 'Июня',
+    6: 'Июля',
+    7: 'Августа',
+    8: 'Сентября',
+    9: 'Октября',
+    10: 'Ноября',
+    11: 'Декабря'
+}
 
+const text = encodeURIComponent(getCurrectTime())
+function formatNumber(number){
+    if (number<10) {
+        return `0${number}`
+    } else {
+        return number
+    }
+}
 function getCurrectTime(){
     const date = new Date()
     const month = date.getMonth()
     const day = date.getDate()
     const hour = date.getHours()
     const minute = date.getMinutes()
-    return `сейчас ${day}.${month+1} около ${hour}:${minute}`
+    return `сейчас ${formatNumber(day)} ${months[month]} около ${formatNumber(hour)}:${formatNumber(minute)}`
 }
 
 function changeStatus(){
