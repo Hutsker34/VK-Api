f1043c5f266d2877b2fa38504307f2554aef8f5f
viacheslav34 <77201329+viacheslav34@users.noreply.github.com>
2021-12-18 15:38:27 +0300

    сделали друзей в статусе

--- index.js
+++ index.js
@@ -1,43 +1,34 @@
 const axios = require('axios');
 const config = require('./config')
 const token = config.token
-const months = {
-    0: 'Января',
-    1: 'Февраля',
-    2: 'Марта',
-    3: 'Апреля',
-    4: 'Мая',
-    5: 'Июня',
-    6: 'Июля',
-    7: 'Августа',
-    8: 'Сентября',
-    9: 'Октября',
-    10: 'Ноября',
-    11: 'Декабря'
+
+
+
+async function friendsOnline(){
+   const result = await axios.get(`https://api.vk.com/method/friends.getOnline?user_id=430099232&online_mobile=0&access_token=${token}&v=5.131`)
+    .then(function (loc){ 
+        return loc.data.response
+    })
+    return `сейчас ${result.length} моих друзей онлайн`
 }
 
-const text = encodeURIComponent(getCurrectTime())
-function formatNumber(number){
-    if (number<10) {
-        return `0${number}`
-    } else {
-        return number
-    }
-}
-function getCurrectTime(){
-    const date = new Date()
-    const month = date.getMonth()
-    const day = date.getDate()
-    const hour = date.getHours()
-    const minute = date.getMinutes()
-    return `сейчас ${formatNumber(day)} ${months[month]} около ${formatNumber(hour)}:${formatNumber(minute)}`
-}
 
-function changeStatus(){
+
+async function changeStatus(){
+    const text = encodeURIComponent( await friendsOnline())
     axios.get(`https://api.vk.com/method/status.set?text=${text}&access_token=${token}&v=5.131`)
     .then(function (loc){
         console.log(loc.data)
     })
 }
-
+friendsOnline().then(function (loc){
+    console.log(` ${loc} `)
+})
 module.exports={changeStatus}
+
+
+
+
+
+
+
